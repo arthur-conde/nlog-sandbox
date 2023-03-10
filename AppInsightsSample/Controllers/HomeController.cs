53d6d232b471e5016553044ae02fcc8230254a3a
Arthur Conde <arthur@taxcycle.com>
2023-03-09 23:20:57 -0700

    feat: app insights sample

---
+++ AppInsightsSample/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using AppInsightsSample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace AppInsightsSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TelemetryClient TelemetryClient { get; }
+        private ILogger<HomeController> Logger { get; }
+
+        public HomeController(ILogger<HomeController> logger, TelemetryClient telemetryClient)
+        {
+            TelemetryClient = telemetryClient;
+            Logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using var op = TelemetryClient.StartOperation<DependencyTelemetry>("Index");
+            op.Telemetry.Type = "Home Controller";
+            Logger.LogInformation("Hello!");
+            Logger.LogWarning("You have been warned, {User}", new { Name = "John Doe" });
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
